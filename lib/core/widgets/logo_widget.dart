bdf8e8860c9e83bc340950c6787d6718a76ce7b0
Ahmed Omran <mamdouhanahamada@gmail.com>
2025-11-19 10:44:52 +0200

    first commit after pr

--- lib/core/widgets/logo_widget.dart
+++ lib/core/widgets/logo_widget.dart
@@ -2,18 +2,11 @@ import 'package:flutter/material.dart';
 
 import '../constants/app_images.dart';
 
-
 class LogoWidget extends StatelessWidget {
   final double? width, height;
   final BoxFit? fit;
   final bool? isWhite;
-  const LogoWidget({
-    this.height,
-    this.width,
-    super.key,
-    this.fit,
-    this.isWhite,
-  });
+  const LogoWidget({this.height, this.width, super.key, this.fit, this.isWhite});
   @override
   Widget build(BuildContext context) {
     return Stack(
