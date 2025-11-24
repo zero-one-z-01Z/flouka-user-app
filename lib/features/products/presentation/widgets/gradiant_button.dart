30f91c8c3833ef5e197647287a92354696ab9266
Ahmed Omran <mamdouhanahamada@gmail.com>
2025-11-24 14:51:32 +0200

    feat: add new `gradiantButton` widget for displaying product seller details and rating.

--- lib/features/products/presentation/widgets/gradiant_button.dart
+++ lib/features/products/presentation/widgets/gradiant_button.dart
@@ -1,8 +1,10 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flouka/core/widgets/svg_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flouka/core/config/app_color.dart';
 import 'package:flouka/core/config/app_styles.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:sizer/sizer.dart';
+import '../../../../core/constants/app_images.dart';
 
 class gradiantButton extends StatelessWidget {
   const gradiantButton({
@@ -17,7 +19,7 @@ class gradiantButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+      margin: const EdgeInsets.symmetric(horizontal: 16),
       padding: const EdgeInsets.all(1),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(5),
@@ -34,19 +36,62 @@ class gradiantButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(5),
         ),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            CachedNetworkImage(
-              imageUrl: "https://placehold.co/600x400/000000/FFFFFF/png",
-              width: 5.w,
-            ),
-            Text(
-              text,
-              style: TextStyleClass.normalStyle().copyWith(
-                color: AppColor.primaryColor,
+            Container(
+              padding: const EdgeInsets.all(2),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: gradiantcolors,
+                ),
+              ),
+              child: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: ClipOval(
+                  child: SvgWidget(svg: Images.apple, width: 8.w, height: 8.w),
+                ),
               ),
             ),
             SizedBox(width: 2.w),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text("Sold By ", style: TextStyleClass.smallStyle()),
+                    SizedBox(width: 1.w),
+                    Text(
+                      "APPLESTORE",
+                      style: TextStyleClass.smallStyle(color: AppColor.primaryColor),
+                    ),
+                  ],
+                ),
+                // SizedBox(height: 1.h),
+                Row(
+                  children: [
+                    Text("4.6", style: TextStyleClass.smallStyle()),
+                    RatingBar(
+                      itemCount: 1,
+                      itemSize: 16.sp,
+                      initialRating: 1,
+                      ratingWidget: RatingWidget(
+                        full: const Icon(Icons.star, color: Colors.amber),
+                        half: const Icon(Icons.star_half, color: Colors.amber),
+                        empty: const Icon(Icons.star_outline, color: Colors.amber),
+                      ),
+                      onRatingUpdate: (value) {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const Spacer(),
+            Icon(Icons.arrow_forward_ios, size: 18.sp),
           ],
         ),
       ),
