1bd71bde679e0e6e8b491d840e9a339120208c19
AhmedOmran22 <mamdouhanahamada@gmail.com>
2026-02-08 11:10:56 +0200

    add name and image in the profile header widget

--- lib/features/auth/presentation/widgets/profile_header_widget.dart
+++ lib/features/auth/presentation/widgets/profile_header_widget.dart
@@ -1,10 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flouka/features/auth/presentation/providers/auth_provider.dart';
 import 'package:flouka/features/auth/presentation/widgets/edit_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
-
 import '../../../../core/config/app_styles.dart';
-import '../../../../core/constants/app_images.dart';
 import '../providers/complete_info_provider.dart';
 
 class ProfileHeaderWidget extends StatelessWidget {
@@ -12,6 +12,7 @@ class ProfileHeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 2.h),
       decoration: BoxDecoration(
@@ -20,10 +21,17 @@ class ProfileHeaderWidget extends StatelessWidget {
       ),
       child: Row(
         children: [
-          CircleAvatar(child: Image.asset(AppImages.preson)),
+          CircleAvatar(
+            radius: 6.w,
+            child: CachedNetworkImage(
+              imageUrl: authProvider.userEntity?.image ?? "",
+              errorWidget: (context, url, error) => const Icon(Icons.error),
+              placeholder: (context, url) => const CircularProgressIndicator(),
+            ),
+          ),
           SizedBox(width: 2.w),
           Text(
-            "MOAZ MOHAMED",
+            authProvider.userEntity?.name ?? "Guest",
             style: TextStyleClass.normalStyle().copyWith(
               fontSize: 15.sp,
               fontWeight: FontWeight.bold,
