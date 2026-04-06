4b66161a4ef2f0c1849078bf1a2e160a49318dd3
AhmedOmran22 <mamdouhanahamada@gmail.com>
2026-04-06 23:58:30 +0200

    deploy

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 android {
     namespace = "com.zeroonez.flouka.user"
     compileSdk = 36
-    ndkVersion = "27.3.13750724"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
